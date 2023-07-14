2c085cf087235a1a786877a0fd89d8869c527e10
nathankrasno <nakrasno@gmail.com>
2023-07-14 00:50:49 -0400

    added flask sqlaclhemy for db connection

--- flask-server/server.py
+++ flask-server/server.py
@@ -3,11 +3,16 @@ from flask_cors import CORS
 import sqlite3
 import datetime
 from courses import populateCourses
+from flask_sqlalchemy import SQLAlchemy
+import os
 
-DATABASE = ''
-
+basedir = os.path.abspath(os.path.dirname(__file__))
 app = Flask(__name__)
-CORS(app)
+app.config['SQLALCHEMY_DATABASE_URI'] =\
+        'sqlite:///' + os.path.join(basedir, 'database.db')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
 
 #Available courses API route
 @app.route("/courses")
@@ -17,6 +22,21 @@ def courses():
 @app.route('/time')
 def get_current_time():
     return {'time:': datetime.datetime.now()}
+
+class Student(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    first = db.Column(db.String(50))
+    last = db.Column(db.String(50))
+    email = db.Column(db.String(50))
+    major = db.Column(db.String(50))
+    choice1 = db.Column(db.String(50))
+    choice2 = db.Column(db.String(50))
+    choice3 = db.Column(db.String(50))
+    courseAssignment = db.Column(db.String(50))
+
+    def __repr__(self):
+        return f"<Student {self.first} {self.last}>"
+
 
 if __name__=="__main__":
     app.run(debug=True)
